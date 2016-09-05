cd9c699b0e64c3b74ed8c33417e20edc5d2aa277
miteyan <miteyan25@gmail.com>
2016-09-05 16:59:06 +0100

    1

---
+++ src/src/Crawl.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+/**
+ * Created by miteyan on 05/09/2016.
+ */
+public class Crawl {
+
+    public static String savePage(String URL) throws IOException {
+        String line ="", all = "";
+        BufferedReader input = null;
+
+        try{
+            URL url = new URL(URL);
+            input = new BufferedReader(new InputStreamReader(url.openStream()));
+
+            while ( (line = input.readLine()) !=null) {
+                all +=line;
+            }
+
+        } finally {
+            if (input != null) {
+                input.close();
+            }
+        }
+
+        return all;
+    }
+
+    public static void main (String[] args) throws IOException {
+        System.out.println(savePage("http://miteyan.com"));
+    }
+
+}
